7ae6b21f43221ae4f7122c4039c59530f6026f32
Pavel Chernov <pvchernoff@gmail.com>
2023-01-22 16:44:07 +0200

    feat: add auth, user, post routes with controllers.

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { verifyToken } from './middleware/auth.js'
+import { register } from './controllers/auth.js'
+import { createPost } from './controllers/posts.js'
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url)
@@ -30,14 +36,25 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
-  }
+  },
 })
 const upload = multer({ storage })
 
+/* Routes with files */
+app.post('/auth/register', upload.single('picture'), register)
+app.post('/posts', verifyToken, upload.single('picture'), createPost)
+
+/* Routes */
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
+app.use('/posts', postRoutes)
+
 /* Mongoose setup */
 const PORT = process.env.PORT || 6001
+const MONGO_URL = process.env.MONGO_URL
 mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO_URL, {
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
