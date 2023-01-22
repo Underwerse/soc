e52d28ae24291541020013f86fc7c360b6d300d6
Pavel Chernov <pvchernoff@gmail.com>
2023-01-22 20:09:48 +0200

    feat: create header.

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import Navbar from '../navbar';
+
 const HomePage = () => {
-  return <div>homepage</div>
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  )
 }
 
-export default HomePage
+export default HomePage;
