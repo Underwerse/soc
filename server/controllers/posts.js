26873c82f7b392fc2264d2ef096b740f82b3929c
Pavel Chernov <pvchernoff@gmail.com>
2023-01-29 20:22:02 +0200

    feat: add UserWidget, complete ProfilePage; fix: fix posts-controller processing likes.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -52,28 +52,27 @@ export const getUserPosts = async (req, res) => {
 
 /* Update */
 export const likePost = async (req, res) => {
-  try{
-    const { id } = req.params
-    const { userId } = req.body
-    const post = await Post.find({ id })
-    const isLiked = post.likes.get(userId)
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
 
     if (isLiked) {
-      post.likes.delete(userId)
+      post.likes.delete(userId);
     } else {
-      post.likes.set(userId, true)
+      post.likes.set(userId, true);
     }
 
     const updatedPost = await Post.findByIdAndUpdate(
       id,
       { likes: post.likes },
       { new: true }
-    )
+    );
 
-    res.status(200).json(updatedPost)
-  } catch(error) {
-    console.error('Error: ' + error.message);
-    res.status(404).json({ message: error.message })
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
-}
+};
 
